2ef4853fc8b00dc528c2e37a45439c3c62bfbeea
minchanwoo <dearmcw0070@gmail.com>
2020-05-08 14:19:13 +0900

    models/index파일에서 development를 production으로 변경해서 cleardb와 연동되는지 확인해보기

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
